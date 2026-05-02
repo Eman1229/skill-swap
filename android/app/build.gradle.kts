3e46b894d2e34b8a01e838f005ccf45225f353a6
Mehrab-Noor <mehrabnoor794@gmail.com>
2026-05-02 02:40:15 -0700

    mobile run

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.skill_swap"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30.0.14904198"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
